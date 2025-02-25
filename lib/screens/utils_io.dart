f38f8368119b2ae47bedb3cc83c7bba38f1ebac5
Rémi Barbier <remi.barbier@hec.edu>
2025-02-25 23:30:41 +0100

    Fix Stripe Web issue + Push latest working version

--- lib/screens/utils_io.dart
+++ lib/screens/utils_io.dart
@@ -14,12 +14,15 @@ bool isMobile() {
          defaultTargetPlatform == TargetPlatform.iOS;
 }
 
+/// Vérifie si l'application tourne sur un desktop (Windows, Mac, Linux)
 bool isDesktop() {
   if (kIsWeb) return false; // Sur Web, on n'est jamais sur un desktop natif
-    return defaultTargetPlatform == TargetPlatform.windows ||
+  return defaultTargetPlatform == TargetPlatform.windows ||
+         defaultTargetPlatform == TargetPlatform.macOS ||
+         defaultTargetPlatform == TargetPlatform.linux; // ✅ Correction ici
 }
 
+/// Vérifie si l'IO est supporté
 bool supportsIO() {
   return !kIsWeb; // Web ne supporte pas `dart:io`
 }
-    
